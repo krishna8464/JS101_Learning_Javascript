f5e557cf7b96c41619e7dfe967389766d212466e
krishna8464 <yendavamshikrishna123@gmail.com>
2022-09-05 20:41:28 +0530

    While-loop

---
+++ Web-22-While-Loop/index.js
@@ -0,0 +1,71 @@
+// let i = 1;
+// let sum = 0;
+// count = 0;
+// while(i<=15){
+//   if (i%2==1){
+//     sum = sum + i;
+//     count = count + 1;
+//   }
+//   i++;
+// }
+// console.log(sum/count);
+// let n = 9
+// if(n%6 == 0){
+//   console.log("hi")
+// }
+// let num = 4;
+// let i = 1;
+// while(i<=10){
+//   console.log(num*i);
+//   i++
+// }
+// let n = 3;
+// let k = 10;
+// let y = 5;
+
+// let mult;
+// let sum = 0;
+// for(let i = 1;i<=k;i++){
+//   mult = n*i;
+//  if(mult%y===0){
+//    sum = sum + mult;
+// }
+
+// }
+// console.log(sum);
+
+// let num = 3;
+
+// for (let i = 1;i<=num;i++){
+//   if(i%3==0 && i%5==0){
+//     console.log("FizzBuzz");
+//   }else if (i%3==0){
+//     console.log("Fizz");
+//   }else if (i%5==0){
+//     console.log("Buzz");
+//   }else{
+//     console.log(i);
+//   }
+// }
+
+ // num = 
+
+ //    let result;
+ //    for(let i = 2;i<num;i++){
+ //        if(num%i ===0){
+ //            result = `No`;
+ //            break;
+ //        }else {
+ //           result = `Yes`;
+ //        }
+ //    }
+ //    console.log(result);
+
+
+
+
+
+
+
+
+
